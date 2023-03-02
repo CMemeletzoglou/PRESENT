d2b316d649860bbf5d73c2e97e4bac90b6ce9768
Zeosleus <zeosleus@protonmail.com>
2023-03-02 11:32:55 +0200

    Added helper package with STATE type declaration

---
+++ rtl/PRESENT/state_pack.vhd
@@ -0,0 +1,5 @@
+package state_pack is
+         type STATE is (RESET, INIT, KEY_GEN, KEYS_READY, OP_ENC, OP_DEC, DONE, INVALID);
+         
+--        type STATE is (RESET, INIT, COUNTING, DONE, INVALID);
+end package;
