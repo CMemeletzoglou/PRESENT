18e30cac4b5ace873276931ce356160b0bcbe295
Zeosleus <zeosleus@protonmail.com>
2023-03-09 12:54:14 +0200

    Removed commented-out code from key memory module file

--- rtl/PRESENT/02_key_mem.vhd
+++ rtl/PRESENT/02_key_mem.vhd
@@ -15,12 +15,7 @@ end entity key_mem;
 
 architecture behavioral of key_mem is
         subtype ROUND_KEY_T is std_logic_vector(63 downto 0);
-
         type ROUND_KEY_MEM is array (0 to 31) of ROUND_KEY_T;
-
-        -- shared variable ram_block : ROUND_KEY_MEM := (others => (others => '0'));
-        -- signal ram_block : ROUND_KEY_MEM := (others => (others => '0'));
-
         signal ram_block : ROUND_KEY_MEM;
 begin
         process (clk, wr_ena)
@@ -31,33 +26,8 @@ begin
                         end if;
                 end if;
         end process;
+
         -- the contents of the memory address indicated by the address bus' value
         -- must always appear on the output data bus        
         data_out <= ram_block(to_integer(unsigned(addr)));
-
-
-        -- port_A : process (clk)
-        -- begin
-        -- 	if rising_edge(clk) then
-        -- 		if (wr_en_a = '1') then -- Port A write operation
-        -- 			ram_block(to_integer(unsigned(addr_a))) := data_in_a;
-        -- 			-- ram_block(to_integer(unsigned(addr_a))) <= data_in_a;
-        -- 		elsif (wr_en_a = '0') then -- Port A read operation
-        -- 		data_out_a <= ram_block(to_integer(unsigned(addr_a)));
-        -- 	      end if;
-        -- 	end if;
-        -- end process port_A;
-
-        -- port_B : process(clk)
-        -- begin
-        -- 	if (rising_edge(clk)) then
-        -- 		if (wr_en_b = '1') then -- Port B write operation
-        -- 			ram_block(to_integer(unsigned(addr_b))) := data_in_b;
-        -- 			-- ram_block(to_integer(unsigned(addr_b))) <= data_in_b;
-        -- 		elsif (wr_en_b = '0') then
-        -- 			data_out_b <= ram_block(to_integer(unsigned(addr_b)));
-        -- 		end if;
-        -- 	end if;
-        -- end process;
-
 end architecture;
