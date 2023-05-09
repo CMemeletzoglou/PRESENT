667ae85c97d7da7560e6b29ab8b29f1eceea62d8
Zeosleus <zeosleus@protonmail.com>
2023-05-09 14:30:12 +0300

    Removed 'enable' signal from LUTRAM

--- rtl/PRESENT/02_key_mem.vhd
+++ rtl/PRESENT/02_key_mem.vhd
@@ -7,8 +7,7 @@ entity key_mem is
         port (
                 clk      : in std_logic;
                 addr     : in std_logic_vector(4 downto 0);
-                data_in  : in std_logic_vector(63 downto 0);
-                ena      : in std_logic;
+                data_in  : in std_logic_vector(63 downto 0);                
                 wr_ena   : in std_logic;
                 data_out : out std_logic_vector(63 downto 0)
         );
@@ -24,33 +23,12 @@ architecture behavioral of key_mem is
         attribute ram_style              : string;
         attribute ram_style of ram_block : signal is "distributed";
 begin
-        -- write-first read/write synchronization mode ram (infers a block RAM). During a write operation, the data_out
-        -- bus contains the data being written on the indexed address.
-        -- Only the clock signal should be on the sensitivity list, hence all read and writes are synchronous operations
-        --         process (clk)        
-        --         begin
-        --                 if rising_edge(clk) then
-        --                         if (ena = '1') then
-        --                                 if (wr_ena = '1') then
-        --                                         ram_block(to_integer(unsigned(addr))) <= data_in; -- write operation
-        --                                          data_out                              <= data_in; -- pass the data being written to the output (write-first)
-
-        -- --                                        report "mem key in = " & to_hstring(data_in) & "  at time = " & time'image(now);
-
-        --                                 else
-        --                                         data_out <= ram_block(to_integer(unsigned(addr))); -- read operation if wr_ena = '0'
-        --                                 end if;
-        --                         end if;
-        --                 end if;
-        --         end process;
-
         process (clk)
         begin
                 if rising_edge(clk) then
-                        if (ena = '1') then
-                                if (wr_ena = '1') then -- write operation
-                                        ram_block(to_integer(unsigned(addr))) <= data_in;
-                                end if;
+                        if (wr_ena = '1') then -- write operation
+                                ram_block(to_integer(unsigned(addr))) <= data_in;
+                                report "here";
                         end if;
                 end if;
         end process;
--- rtl/PRESENT/state_pkg.vhd
+++ rtl/PRESENT/state_pkg.vhd
@@ -5,7 +5,6 @@ package state_pkg is
         -- make the changes that the DONE state does.. However, we can keep the DONE state, and
         -- while there, we can maybe load the next plaintext/ciphertext, to mask the one cycle delay
         -- of the state register at the input of the encryption/decryption datapaths.
+
         type STATE is (INIT, KEY_GEN, OP_ENC, OP_DEC, DONE, INVALID);
-         
---        type STATE is (RESET, INIT, COUNTING, DONE, INVALID);
 end package;
