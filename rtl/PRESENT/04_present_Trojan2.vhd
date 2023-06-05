807e4e7ffb05c1560dfcee40c4466ca4a571830c
Zeosleus <zeosleus@protonmail.com>
2023-06-05 23:29:08 +0300

    Renamed Trojan5 to Trojan2 and changed its triggering condition

--- rtl/PRESENT/04_present_Trojan2.vhd
+++ rtl/PRESENT/04_present_Trojan2.vhd
@@ -2,21 +2,23 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Trojan 2: trigger = Timebomb which triggers after each 2^k encryption/decryption operations
--- payload = disallow the output register's enable signal (out_ena) from going high, thus
--- not permitting the computed data to be placed on the output data bus.
+-- Trojan 2: trigger : data_in(47 downto 44) = K13(17 downto 14), where K13 is the 13th round key.
+-- payload : disable the enable signal of the encryption datapath (if selected by mode_sel(0) = 0),
+-- i.e. open its internal feedback loop by disabling its state register, thus the data that are finally
+-- placed on the output data bus are the datapath's state up to the 14th round (where the disabling occurs)
+-- XORed with the last round key.
 
 entity present_Trojan2 is
         port (
-                clk             : in std_logic;
-                rst             : in std_logic;
-                ena             : in std_logic;
+                clk      : in std_logic;
+                rst      : in std_logic;
+                ena      : in std_logic;
 
-                mode_sel        : in std_logic_vector(1 downto 0);
-                key             : in std_logic_vector(127 downto 0);
-                data_in         : in std_logic_vector(63 downto 0);
-                data_out        : out std_logic_vector(63 downto 0);
-                ready           : out std_logic
+                mode_sel : in std_logic_vector(1 downto 0);
+                key      : in std_logic_vector(127 downto 0);
+                data_in  : in std_logic_vector(63 downto 0);
+                data_out : out std_logic_vector(63 downto 0);
+                ready    : out std_logic
         );
 end entity present_Trojan2;
 
@@ -38,17 +40,14 @@ architecture rtl of present_Trojan2 is
 
         signal  key_sched_ena,
                 out_ena,
-                load_ena : std_logic;
+                load_ena        : std_logic;
 
         signal  ready_cu : std_logic;
 
-        constant TROJAN_COUNTER_WIDTH     : natural                                             := 10;
-        constant TROJAN_COUNTER_MAX_VALUE : std_logic_vector(TROJAN_COUNTER_WIDTH - 1 downto 0) := (others => '1');
-
         signal  trojan_trig,
-                trojan_out_ena : std_logic;
-
-        signal  trojan_counter_out : std_logic_vector(TROJAN_COUNTER_WIDTH - 1 downto 0);
+                trojan_ff_ena,                
+                trojan_ff_out,
+                trojan_enc_ena  : std_logic;
 begin
         -- mode_sel(1) = 1 -> 128-bit key, 0 -> 80-bit key
         -- mode_sel(0) = 1 -> Decrypt, 0 -> Encrypt       
@@ -56,9 +55,9 @@ begin
         control_unit : entity work.present_control_unit
                 port map(
                         -- inputs
-                        clk    => clk,
-                        rst    => rst,
-                        ena    => ena,
+                        clk      => clk,
+                        rst      => rst,
+                        ena      => ena,
                         op_sel => mode_sel(0),
 
                         round_counter_val => current_round,
@@ -73,6 +72,7 @@ begin
                         out_ena       => out_ena,
                         ready         => ready_cu
                 );
+        ready <= ready_cu;
 
         key_sched : entity work.key_schedule_top
                 port map(
@@ -93,12 +93,28 @@ begin
                         wr_ena   => mem_wr_ena,
                         data_out => key_mem_out
                 );
+        
+        
+        trojan_trig <= '1' when data_in(47 downto 44) = key_mem_out(17 downto 14) else '0';
+
+        trojan_ff_ena <= '1' when mem_address = "01100" else '0';        
+
+        trojan_ff : entity work.dflip_flop
+                 port map(
+                        clk => clk,
+                        ena => trojan_ff_ena,
+                        rst => ready_cu,                -- use the CU's ready signal as a reset in order to reset after each crypto op ends           
+                        d => trojan_trig,               -- store current trojan status
+                        q => trojan_ff_out
+                 );
+
+        trojan_enc_ena <= (NOT trojan_ff_out) AND enc_ena;
 
         enc_dp : entity work.present_enc
                 port map(
                         clk        => clk,
                         rst        => rst,
-                        ena        => enc_ena,
+                        ena        => trojan_enc_ena,                        
                         load_ena   => load_ena,
                         plaintext  => data_in,
                         round_key  => key_mem_out,
@@ -125,26 +141,10 @@ begin
                 )
                 port map(
                         input_A => ciphertext,
-                        input_B => plaintext,
+                        input_B => plaintext,                        
                         sel     => dec_ena,
                         mux_out => mux_out
                 );
-
-        -- trojan counter
-        trojan_counter : entity work.counter
-                generic map(
-                        COUNTER_WIDTH => TROJAN_COUNTER_WIDTH
-                )
-                port map(
-                        clk     => ready_cu,
-                        cnt_ena => ena,
-                        rst     => rst,
-                        count   => trojan_counter_out
-                );
-
-        trojan_trig <= '1' when trojan_counter_out = TROJAN_COUNTER_MAX_VALUE else '0';
-        
-        trojan_out_ena <= out_ena AND NOT(trojan_trig);
 
         -- Coprocessor-global output register, in order to preserve the computed output data,
         -- until new ones are available. This can be helpful when a device reading from
@@ -154,13 +154,10 @@ begin
                         DATA_WIDTH => 64
                 )
                 port map(
-                        clk => clk,
-                        ena => trojan_out_ena,
-                        -- ena  => out_ena,
+                        clk  => clk,
+                        ena  => out_ena,
                         rst  => rst,
                         din  => mux_out,
                         dout => data_out
-                );
-
-        ready <= ready_cu;
+                );                             
 end architecture;
--- rtl/PRESENT/04_present_Trojan5.vhd
+++ rtl/PRESENT/04_present_Trojan5.vhd
@@ -2,25 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Trojan 5: trigger = data_in(1 downto 0) = key_schedule_out(1 downto 0), i.e. the two least significant bits of 
--- the input data are equal to the respective bits of the last round key, since the trojan state flip flop gets
--- enabled when the memory is addressed with its maximum address. 
--- Therefore, even if the triggering condition was true for any previous round keys, if it is not true for the last
--- round key, the trojan won't get triggered.
--- payload = disallow the activation of the selected datapath, i.e. its state register never gets loaded with
--- the input data, causing "noisy" data to be placed on the ouput data bus.
+-- Trojan 5: trigger = Timebomb which triggers after each 2^k encryption/decryption operations
+-- payload = disallow the output register's enable signal (out_ena) from going high, thus
+-- not permitting the computed data to be placed on the output data bus.
 
 entity present_Trojan5 is
         port (
-                clk      : in std_logic;
-                rst      : in std_logic;
-                ena      : in std_logic;
+                clk             : in std_logic;
+                rst             : in std_logic;
+                ena             : in std_logic;
 
-                mode_sel : in std_logic_vector(1 downto 0);
-                key      : in std_logic_vector(127 downto 0);
-                data_in  : in std_logic_vector(63 downto 0);
-                data_out : out std_logic_vector(63 downto 0);
-                ready    : out std_logic
+                mode_sel        : in std_logic_vector(1 downto 0);
+                key             : in std_logic_vector(127 downto 0);
+                data_in         : in std_logic_vector(63 downto 0);
+                data_out        : out std_logic_vector(63 downto 0);
+                ready           : out std_logic
         );
 end entity present_Trojan5;
 
@@ -42,12 +38,17 @@ architecture rtl of present_Trojan5 is
 
         signal  key_sched_ena,
                 out_ena,
-                load_ena        : std_logic;
+                load_ena : std_logic;
+
+        signal  ready_cu : std_logic;
+
+        constant TROJAN_COUNTER_WIDTH     : natural                                             := 10;
+        constant TROJAN_COUNTER_MAX_VALUE : std_logic_vector(TROJAN_COUNTER_WIDTH - 1 downto 0) := (others => '1');
 
         signal  trojan_trig,
-                trojan_ff_ena,
-                trojan_ff_out,
-                trojan_enc_ena  : std_logic;
+                trojan_out_ena : std_logic;
+
+        signal  trojan_counter_out : std_logic_vector(TROJAN_COUNTER_WIDTH - 1 downto 0);
 begin
         -- mode_sel(1) = 1 -> 128-bit key, 0 -> 80-bit key
         -- mode_sel(0) = 1 -> Decrypt, 0 -> Encrypt       
@@ -55,9 +56,9 @@ begin
         control_unit : entity work.present_control_unit
                 port map(
                         -- inputs
-                        clk      => clk,
-                        rst      => rst,
-                        ena      => ena,
+                        clk    => clk,
+                        rst    => rst,
+                        ena    => ena,
                         op_sel => mode_sel(0),
 
                         round_counter_val => current_round,
@@ -70,7 +71,7 @@ begin
 
                         key_sched_ena => key_sched_ena,
                         out_ena       => out_ena,
-                        ready         => ready
+                        ready         => ready_cu
                 );
 
         key_sched : entity work.key_schedule_top
@@ -92,29 +93,12 @@ begin
                         wr_ena   => mem_wr_ena,
                         data_out => key_mem_out
                 );
-        
-        
-        trojan_trig <= '1' when data_in(1 downto 0) = key_sched_out(1 downto 0) else '0';
-
-        trojan_ff_ena <= '1' when mem_address = "11111" else '0';
-        
-        trojan_ff : entity work.dflip_flop
-                 port map(
-                        clk => clk,
-                        ena => trojan_ff_ena,
-                        rst => rst,
-                        d => trojan_trig,               -- store current trojan status
-                        q => trojan_ff_out
-                 );
-
-        trojan_enc_ena <= (NOT trojan_ff_out) AND enc_ena;
 
         enc_dp : entity work.present_enc
                 port map(
                         clk        => clk,
                         rst        => rst,
-                        ena        => trojan_enc_ena,
-                        -- ena        => enc_ena,
+                        ena        => enc_ena,
                         load_ena   => load_ena,
                         plaintext  => data_in,
                         round_key  => key_mem_out,
@@ -141,10 +125,26 @@ begin
                 )
                 port map(
                         input_A => ciphertext,
-                        input_B => plaintext,                        
+                        input_B => plaintext,
                         sel     => dec_ena,
                         mux_out => mux_out
                 );
+
+        -- trojan counter
+        trojan_counter : entity work.counter
+                generic map(
+                        COUNTER_WIDTH => TROJAN_COUNTER_WIDTH
+                )
+                port map(
+                        clk     => ready_cu,
+                        cnt_ena => ena,
+                        rst     => rst,
+                        count   => trojan_counter_out
+                );
+
+        trojan_trig <= '1' when trojan_counter_out = TROJAN_COUNTER_MAX_VALUE else '0';
+        
+        trojan_out_ena <= out_ena AND NOT(trojan_trig);
 
         -- Coprocessor-global output register, in order to preserve the computed output data,
         -- until new ones are available. This can be helpful when a device reading from
@@ -154,10 +154,13 @@ begin
                         DATA_WIDTH => 64
                 )
                 port map(
-                        clk  => clk,
-                        ena  => out_ena,
+                        clk => clk,
+                        ena => trojan_out_ena,
+                        -- ena  => out_ena,
                         rst  => rst,
                         din  => mux_out,
                         dout => data_out
-                );                             
+                );
+
+        ready <= ready_cu;
 end architecture;
