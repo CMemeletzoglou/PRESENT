ec55175809bda4f1005c10f6b26f7b3b52c9abdb
Zeosleus <zeosleus@protonmail.com>
2023-06-08 10:36:06 +0300

    Updated Trojan6 testbench to include entity's generic for the trojan counter's width

--- rtl/PRESENT/present_Trojan6_tb.vhd
+++ rtl/PRESENT/present_Trojan6_tb.vhd
@@ -9,43 +9,50 @@ end;
 architecture bench of present_Trojan6_tb is
 
         component present_Trojan6
+                generic (
+                        TROJAN_COUNTER_WIDTH : natural
+                );
                 port (
-                        clk            : in std_logic;
-                        rst            : in std_logic;
-                        ena            : in std_logic;
-                        mode_sel       : in std_logic_vector(1 downto 0);
-                        key            : in std_logic_vector(127 downto 0);
-                        data_in        : in std_logic_vector(63 downto 0);
-                        data_out       : out std_logic_vector(63 downto 0);
-                        ready          : out std_logic
+                        clk                : in std_logic;
+                        rst                : in std_logic;
+                        ena                : in std_logic;
+                        mode_sel           : in std_logic_vector(1 downto 0);
+                        key                : in std_logic_vector(127 downto 0);
+                        data_in            : in std_logic_vector(63 downto 0);
+                        data_out           : out std_logic_vector(63 downto 0);
+                        ready              : out std_logic
                 );
         end component;
 
         -- Clock period
         constant clk_period : time := 5 ns;
         -- Generics
+        constant TROJAN_COUNTER_WIDTH : natural := 10;
 
         -- Ports
-        signal clk            : std_logic;
-        signal rst            : std_logic;
-        signal ena            : std_logic;
-        signal mode_sel       : std_logic_vector(1 downto 0);
-        signal key            : std_logic_vector(127 downto 0);
-        signal data_in        : std_logic_vector(63 downto 0);
-        signal data_out       : std_logic_vector(63 downto 0);
-        signal ready          : std_logic;
+        signal clk                : std_logic;
+        signal rst                : std_logic;
+        signal ena                : std_logic;
+        signal mode_sel           : std_logic_vector(1 downto 0);
+        signal key                : std_logic_vector(127 downto 0);
+        signal data_in            : std_logic_vector(63 downto 0);
+        signal data_out           : std_logic_vector(63 downto 0);
+        signal ready              : std_logic;
 begin
 
         present_Trojan6_inst : present_Trojan6
+        generic map(
+                TROJAN_COUNTER_WIDTH => TROJAN_COUNTER_WIDTH
+        )
         port map(
-                clk            => clk,
-                rst            => rst,
-                ena            => ena,
-                mode_sel       => mode_sel,
-                key            => key,
-                data_in        => data_in,
-                data_out       => data_out,
-                ready          => ready
+                clk                => clk,
+                rst                => rst,
+                ena                => ena,
+                mode_sel           => mode_sel,
+                key                => key,
+                data_in            => data_in,
+                data_out           => data_out,
+                ready              => ready
         );
 
         clk_process : process
@@ -72,17 +79,17 @@ begin
                         return slv;
                 end function;
 
-                variable fill : std_logic_vector(47 downto 0) := x"000000000000";
+                variable fill : std_logic_vector(47 downto 0) := x"000000000000";                
         begin                   
                 rst <= '1', '0' after clk_period;
                 ena <= '0', '1' after clk_period;
 
-                mode_sel <= b"00"; -- 128-bit encryption
+                mode_sel <= b"00"; -- 80-bit encryption
                 
-                key <= fill & rand_slv(80);
-                data_in <= rand_slv(64);                
+                key <= fill & rand_slv(80);              
+                data_in <= rand_slv(64);                                
 
-                rand_slv_gen_loop : for i in 0 to 8191 loop                               
+                rand_slv_gen_loop_enc : for i in 0 to 4095 loop                               
                         wait for 33 * clk_period;
                         
                         if i = 0 then
@@ -91,8 +98,21 @@ begin
                         end if;
 
                         data_in <= rand_slv(64);
-                end loop rand_slv_gen_loop;
+
+                        if i = 4095 then
+                                wait for 32 * clk_period;
+                        end if;
+                end loop rand_slv_gen_loop_enc;
+                
+                mode_sel <= b"01"; -- switch to 80-bit decryption
+
+                rand_slv_gen_loop_dec : for i in 0 to 4095 loop
+                        data_in <= rand_slv(64);
+                        
+                        wait for 33 * clk_period;
+                end loop rand_slv_gen_loop_dec;
 
                 wait; -- halt the simulation
         end process stimuli_proc;
+
 end;
